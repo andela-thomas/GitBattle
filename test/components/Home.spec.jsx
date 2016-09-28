757ddea9645350569cf81feba8ccfece1403f04c
Thomas Nyambati <thomas.nyambati@andela.com>
2016-09-28 22:35:50 +0100

    Beautify code

--- test/components/Home.spec.jsx
+++ test/components/Home.spec.jsx
@@ -1,40 +1,46 @@
-import { shallow } from 'enzyme';
+import {shallow} from 'enzyme';
 import React from 'react';
-import Home  from '../../app/components/Home.jsx';
+import Home from '../../app/components/Home.jsx';
 import {expect} from 'chai';
 
 describe('<Home/>', () => {
   let HomeWrapper;
-  beforeEach(()=> {
+  beforeEach(() => {
     HomeWrapper = shallow(<Home/>);
   });
 
-  it('should render without exploding', ( )=> {
-    expect(HomeWrapper.length).to.equal(1);
+  it('should render without exploding', () => {
+    expect(HomeWrapper.length)
+      .to
+      .equal(1);
   });
 
   it('should render MainContainer component', () => {
-    expect(HomeWrapper.find('MainContainer').length).to.equal(1);
+    expect(HomeWrapper.find('MainContainer').length)
+      .to
+      .equal(1);
   });
 
   it('should have all the expected elements', () => {
-    const elements = [
-      <h1> Github Battles </h1>,
-      <p className="lead"> Some fancy motto</p>
-    ];
-    expect(HomeWrapper.contains(elements)).to.equal(true);
+    const elements = [< h1 > Github Battles < /h1>,
+      <p className="lead"> Some fancy motto</p >];
+    expect(HomeWrapper.contains(elements))
+      .to
+      .equal(true);
   });
 
-  it('should contain a link', ()=> {
-    const LinkWrapper =HomeWrapper.find('Link');
-    expect(LinkWrapper.length).to.equal(1);
-    expect(
-      LinkWrapper.contains(
-        <button className="btn btn-lg btn-success">Get Started</button>
-      )
-    );
+  it('should contain a link', () => {
+    const LinkWrapper = HomeWrapper.find('Link');
+    expect(LinkWrapper.length)
+      .to
+      .equal(1);
+    expect(LinkWrapper.contains(
+      <button className="btn btn-lg btn-success">Get Started</button>
+    ));
 
-    expect(LinkWrapper.prop('to')).to.equal('/playerOne');
+    expect(LinkWrapper.prop('to'))
+      .to
+      .equal('/playerOne');
 
   });
 
--- test/components/Main.specs.jsx
+++ test/components/Main.specs.jsx
@@ -1,21 +1,24 @@
-import { shallow } from 'enzyme';
+import {shallow} from 'enzyme';
 import React from 'react';
 import Main from '../../app/components/Main.jsx';
-import { expect } from 'chai';
+import {expect} from 'chai';
 
 describe('<Main/>', () => {
   context('When the component is shallow rendered', () => {
 
     let MainWrapper;
-    let props = { pathname: "/jsref/prop_loc_pathname.asp" };
+    let props = {
+      pathname: "/jsref/prop_loc_pathname.asp"
+    };
 
     before(() => {
-      MainWrapper = shallow( < Main location = { props }
-        />);
+      MainWrapper = shallow(< Main location = {
+        props
+      } />);
     });
 
     it('Should render without exploading', () => {
-    console.log(MainWrapper);
+      console.log(MainWrapper);
       // expect(MainWrapper.length).to.equal(1);
     });
   });
