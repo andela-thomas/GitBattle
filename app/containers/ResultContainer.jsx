7a647e2e3305f57ee34c550806c6358ca66882f5
Thomas Nyambati <thomas.nyambati@andela.com>
2016-09-28 22:21:16 +0100

    Add results container

---
+++ app/containers/ResultContainer.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import Results from '../components/Results.jsx';
+import githubHelpers from '../utils/github-helper.jsx';
+
+export default class ResultsContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      scores: []
+    };
+  }
+
+  componentDidMount() {
+    githubHelpers.battle(this.props.location.state.playersInfo)
+      .then(function (scores) {
+        this.setState({
+          scores: scores,
+          isLoading: false
+        });
+      }.bind(this));
+  }
+
+  render() {
+    return (
+      <Results
+        isLoading={this.state.isLoading}
+        playersInfo={this.props.location.state.playersInfo}
+        scores={this.state.scores} />
+    );
+  }
+}
