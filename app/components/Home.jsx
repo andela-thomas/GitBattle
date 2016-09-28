72a4a53b1b0e28f68da9be1758db72fa63e3bef2
Thomas Nyambati <thomas.nyambati@andela.com>
2016-09-28 22:12:51 +0100

    Add home component

---
+++ app/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link} from 'react-router';
+import MainContainer  from './MainContainer.jsx';
+
+export default function Home () {
+    return (
+      <MainContainer>
+        <h1> Github Battles </h1>
+        <p className="lead"> Some fancy motto</p>
+        <Link to="/playerOne">
+            <button className="btn btn-lg btn-success">Get Started</button>
+        </Link>
+      </MainContainer>
+    );
+}
